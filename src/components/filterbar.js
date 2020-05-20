c88ba10c1833d41355e63cba6693c61caea27215
José Luis Valdiviezo Peña <luisj.valdiviezo20@gmail.com>
2020-05-20 17:23:21 -0500

    First Day

---
+++ src/components/filterbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class FilterReset extends React.Component{
+  render(){
+    return(
+      <button className="" id="buttonReset">
+        <svg className="change-color bi bi-x-square-fill" width="2em" height="2em " viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2zm9.854 4.854a.5.5 0 00-.708-.708L8 7.293 4.854 4.146a.5.5 0 10-.708.708L7.293 8l-3.147 3.146a.5.5 0 00.708.708L8 8.707l3.146 3.147a.5.5 0 00.708-.708L8.707 8l3.147-3.146z" clipRule="evenodd"/>
+        </svg>
+      </button>
+    )
+  }
+}
+class FilterButton extends React.Component{
+  render(){
+    return(
+      <button className="" id="filterButton">
+        {this.props.name}
+      </button>
+    )
+  }
+}
+export default class FilterBar extends React.Component{
+  render(){
+    return(
+      <div className="container" id="filterbar">
+       <div>
+          <FilterButton name="JavaScript"/>
+          <FilterReset/>
+       </div>
+      </div>
+    )
+  }
+}
